f726e700dd7f44410cdffa99eaae877f8ea3e849
Supernova <142123536+nova524@users.noreply.github.com>
2024-02-29 22:54:53 +0900

    슈퍼노바

--- app/src/main/java/com/objective/objectnet/MainActivities/MainActivity.kt
+++ app/src/main/java/com/objective/objectnet/MainActivities/MainActivity.kt
@@ -5,23 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import com.objective.objectnet.R
 import com.objective.objectnet.login.Login
 
+// MainActivity 클래스를 정의합니다. 이 클래스는 AppCompatActivity를 상속받습니다.
 class MainActivity : AppCompatActivity() {
+    // onCreate 메서드를 오버라이드하여 액티비티가 생성될 때 호출될 코드를 정의합니다.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 화면을 activity_main 레이아웃으로 설정합니다.
         setContentView(R.layout.activity_main)
 
-        // Login 클래스의 인스턴스 생성
+        // Login 클래스의 인스턴스를 생성합니다.
         val login = Login()
-        //에베베베
-        // 예시로 로그인 메서드 호출
+
+        // 예시로 사용할 사용자 이름과 비밀번호를 정의합니다.
         val username = "example_user"
         val password = "example_password"
+
+        // Login 클래스의 loginUser 메서드를 호출하여 사용자를 로그인시킵니다.
         val isLoggedIn = login.loginUser(username, password)
 
+        // 만약 로그인에 성공했다면 다음 코드를 실행합니다.
         if (isLoggedIn) {
-            // 로그인에 성공한 경우 처리할 코드
+            // TODO: 로그인에 성공한 경우 처리할 코드를 여기에 작성합니다.
         } else {
-            // 로그인에 실패한 경우 처리할 코드
+            // TODO: 로그인에 실패한 경우 처리할 코드를 여기에 작성합니다.
         }
     }
 }
--- app/src/main/java/com/objective/objectnet/login/Login.kt
+++ app/src/main/java/com/objective/objectnet/login/Login.kt
@@ -2,7 +2,6 @@ package com.objective.objectnet.login
 
 class Login {
     fun loginUser(username: String, password: String): Boolean {
-        // 로그인 로직은 니가 구현해^^!!
         return true
     }
 }
