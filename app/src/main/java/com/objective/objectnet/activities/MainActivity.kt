20b8885d89570d9b2e001e123df424d05d45cd49
PUKO_ <porksodeli11@gmail.com>
2024-02-29 11:57:35 +0900

    fddfsdfsd

---
+++ app/src/main/java/com/objective/objectnet/activities/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.objective.objectnet.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.objective.objectnet.R
+import com.objective.objectnet.login.Login
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Login 클래스의 인스턴스 생성
+        val login = Login()
+        //에베베베
+        // 예시로 로그인 메서드 호출
+        val username = "example_user"
+        val password = "example_password"
+        val isLoggedIn = login.loginUser(username, password)
+
+        if (isLoggedIn) {
+            // 로그인에 성공한 경우 처리할 코드
+        } else {
+            // 로그인에 실패한 경우 처리할 코드
+        }
+    }
+}
--- app/src/main/java/com/objective/objectnet/login/Login.kt
+++ app/src/main/java/com/objective/objectnet/login/Login.kt
@@ -1,2 +1,8 @@
 package com.objective.objectnet.login
 
+class Login {
+    fun loginUser(username: String, password: String): Boolean {
+        // 로그인 로직은 니가 구현해^^!!
+        return true
+    }
+}
